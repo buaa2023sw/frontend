d8de2ae829f2949c1c18b5781c84647519e2cd93
“pplrabbit” <1505779897@qq.com>
2023-04-18 17:01:22 +0800

    save

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,35 +1,39 @@
 <template>
-<v-app>
-  <v-container>
-    <v-row>
-      <v-col cols="3" class=""></v-col>
-      <v-col cols="6">
-        <v-text-field label="Email" v-model="inputUserEmail"></v-text-field>
-        <v-text-field label="Password" type="password" v-model="inputUserPwd"></v-text-field>
+  <v-container fluid>
+    <v-row align="center" justify="center" class="blue lighten-5" style="min-height: calc(100vh - 64px);">
+      <v-col cols="12" sm="8" md="6" lg="4" class="d-flex justify-center">
+        <v-card class="elevation-12" style="max-width: 500px;">
+          <v-card-text class="mt-4 mb-4">
+            <v-form @submit.prevent="login">
+              <v-row>
+                <v-col cols="12" class="mb-3">
+                  <v-text-field label="用户名" v-model="inputUserName" outlined dense></v-text-field>
+                </v-col>
+                <v-col cols="12" class="mb-3">
+                  <v-text-field label="密码" v-model="inputUserPwd" outlined dense type="password"></v-text-field>
+                </v-col>
+                <v-col cols="12" class="mb-3">
+                  <v-btn :disabled="busy" color="blue darken-2" class="white--text" block @click="login">登录</v-btn>
+                </v-col>
+                <v-col cols="12" class="text-center">
+                  <span class="caption grey--text text--darken-1">没有账号？</span>
+                  <v-btn :disabled="busy" color="transparent" class="grey--text text--darken-1" text @click="$router.push('/register')">注册</v-btn>
+                </v-col>
+              </v-row>
+            </v-form>
+          </v-card-text>
+        </v-card>
       </v-col>
-      <v-col cols="3" class=""></v-col>
-    </v-row>
-    <v-row class="">
-      <v-col cols="3"></v-col>
-      <v-col cols="2">
-        <v-btn :disabled="busy" block @click="login()">login</v-btn>
-      </v-col>
-      <v-col cols="2"></v-col>
-      <v-col cols="2">
-        <v-btn :disabled="busy" block link to="/register">register</v-btn>
-      </v-col>
-      <v-col cols="3"></v-col>
     </v-row>
   </v-container>
-
-</v-app>
 </template>
 
 <script>
 import Cookies from 'js-cookie'
+import axios from "axios";
 
 if (Cookies.get('user') !== undefined) {
-  alert('already logged in!');
+  alert('您已处于登录状态');
   window.location.href = '/home';
 }
 
@@ -38,16 +42,41 @@ export default {
   data() {
     return {
       busy : false,
-      inputUserEmail: null,
+      inputUserName: null,
       inputUserPwd: null
     }
   },
   methods: {
     async login() {
-      this.busy = true
-      let inputUserEmail = this.inputUserEmail
+      // this.busy = true
+      console.log(JSON.stringify({
+        id: 1,
+        name: 'TrickEye',
+        email: 'trickeye@buaa.edu.cn',
+        projects: [{
+          id: 1,
+          name: 'Project1'
+        }, {
+          id: 2,
+          name: 'Project2'
+        }]
+      }))
+      let inputUserName = this.inputUserName
       let inputUserPwd = this.inputUserPwd
-      this.inputUserPwd = null
+      let data = "{\"username\":" + inputUserName + ", \"password\":" + inputUserPwd + "}"
+      console.log(data)
+      axios.post("/api/management/showUsers", JSON.parse(data))
+          .then((response) => {
+            console.log(response)
+            this.userMessages = response.data.users
+          })
+          .catch((err) => {
+            console.error(err);
+            this.userMessages = null
+          })
+
+
+
       setTimeout(() => {
         this.busy = false
         if (inputUserEmail === 'trickeye@buaa.edu.cn') {
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -4,9 +4,10 @@
       <v-row>
         <v-col cols="3" class=""></v-col>
         <v-col cols="6">
-          <v-text-field label="Email" v-model="inputUserEmail"></v-text-field>
-          <v-text-field label="Password" type="password" v-model="inputUserPwd"></v-text-field>
-          <v-text-field label="Confirm Password" type="password" v-model="confirmUserPwd"></v-text-field>
+          <v-text-field label="用户名" v-model="username"></v-text-field>
+          <v-text-field label="邮箱" v-model="email"></v-text-field>
+          <v-text-field label="密码" type="password" v-model="password"></v-text-field>
+          <v-text-field label="确认密码" type="password" v-model="confirmPassword"></v-text-field>
         </v-col>
         <v-col cols="3" class=""></v-col>
       </v-row>
@@ -27,17 +28,35 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "Register",
   data() {
     return {
-      inputUserEmail: '',
-      inputUserPwd: '',
-      confirmUserPwd: ''
+      username: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
     }
   },
   methods: {
     register() {
+      let data = JSON.stringify({
+        username: this.username,
+        email: this.email,
+        password: this.password
+      })
+      // console.log(data)
+      axios.post("/api/register", JSON.parse(data))
+          .then((response) => {
+            console.log(response)
+            this.userMessages = response.data.users
+          })
+          .catch((err) => {
+            console.error(err);
+            this.userMessages = null
+          })
       console.log('ok, I know your email is ' + this.inputUserEmail +
           ' and your user pwd is ' + this.inputUserPwd +
           ' and your confirm pwd is ' + this.confirmUserPwd)
