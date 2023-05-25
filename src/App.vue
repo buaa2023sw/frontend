da4835a5a5fa0dea87bed4648e0006922dc12b94
“pplrabbit” <1505779897@qq.com>
2023-05-25 16:28:25 +0800

    删去管理端的任务提醒显示

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
 
 
       <v-icon v-if="false" style="right: 1%">mdi-bell</v-icon>
-      <v-icon v-if="user" style="right: 0%" @click="checkClock">mdi-clock-outline</v-icon>
+      <v-icon v-if="existUser()" style="right: 0%" @click="checkClock">mdi-clock-outline</v-icon>
       <v-tooltip bottom>
         <template v-slot:activator="{ on, attrs }">
           <v-btn link to="/allProject/" v-if="existUser()" icon color="white" v-bind="attrs" v-on="on">
